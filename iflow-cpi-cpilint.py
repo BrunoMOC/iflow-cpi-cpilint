1cdf30024a4ec218fc5ab9e70152fa312d413890
Bruno <bopenkoski@gmail.com>
2024-02-29 00:08:19 -0300

    Changes to CPI NEO V1

--- iflow-cpi-cpilint.py
+++ iflow-cpi-cpilint.py
@@ -2,6 +2,8 @@ import requests
 from concurrent.futures import ThreadPoolExecutor
 from lxml import etree
 import subprocess
+import base64
+
 
 def get_bearer_token(auth_api_url, client_id, client_secret):
     data = {
@@ -16,7 +18,7 @@ def get_bearer_token(auth_api_url, client_id, client_secret):
         print(f"Failed to authenticate. Status code: {response.status_code}")
         return None
 
-def get_package_names(api_url_get_packs, bearer_token):
+def get_package_names(api_url_get_packs, bearer_token=-1, s_user=-1, password=-1):
     headers = {"Authorization": f"Bearer {bearer_token}"}
     response = requests.get(api_url_get_packs, headers=headers)
     if response.status_code == 200:
@@ -25,6 +27,56 @@ def get_package_names(api_url_get_packs, bearer_token):
     else:
         print("Error getting package names")
         return []
+
+
+
+def get_package_names_neo(api_url_get_packs, s_user=-1, password=-1):
+
+    credentials = f"{s_user}:{password}"
+    credentials_b64 = base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
+    
+    headers = {
+        'Authorization': 'Basic ' + credentials_b64
+    }
+    response = requests.get(api_url_get_packs, headers=headers)
+    if response.status_code == 200:
+        root = etree.fromstring(response.content)
+        return [entry.text for entry in root.findall('.//d:Id', namespaces={'d': 'http://schemas.microsoft.com/ado/2007/08/dataservices'})]
+    else:
+        print("Error getting package names")
+        return []
+
+
+def get_iflows_names_neo(package_id, api_url_get_iflow, s_user=-1, password=-1):
+
+    credentials = f"{s_user}:{password}"
+    credentials_b64 = base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
+    headers = {'Authorization': 'Basic ' + credentials_b64}
+    
+    api_endpoint = api_url_get_iflow.format(id=package_id)
+    response = requests.get(api_endpoint, headers=headers)
+    if response.status_code == 200:
+        root = etree.fromstring(response.content)
+        return [entry.text for entry in root.findall('.//d:Id', namespaces={'d': 'http://schemas.microsoft.com/ado/2007/08/dataservices'})]
+    else:
+        print(f"Error getting iflows for package {package_id}")
+        return []
+
+
+def download_zip_file_neo(api_url_download, id_value, version_value, s_user=-1, password=-1):
+    api_endpoint = api_url_download.format(id=id_value, version=version_value)
+    # headers = {"Authorization": f"Bearer {bearer_token}"}
+    credentials = f"{s_user}:{password}"
+    credentials_b64 = base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
+    headers = {'Authorization': 'Basic ' + credentials_b64}
+    
+    response = requests.get(api_endpoint, headers=headers)
+    if response.status_code == 200:
+        with open(f"{id_value}.zip", "wb") as zip_file:
+            zip_file.write(response.content)
+        print("File downloaded successfully.")
+    else:
+        print(f"Failed to download file. Status code: {response.status_code}")
 
 def get_iflows_names(package_id, api_url_get_iflow, bearer_token):
     headers = {"Authorization": f"Bearer {bearer_token}"}
@@ -65,28 +117,56 @@ def run_cpi_lint(array_iflow_names, rules_xml_path):
 
 if __name__ == "__main__":
     print("Run this code in 'files' folder")
-    auth_api_url = input("Enter the authentication API URL: ")
-    client_id = input("Enter your client ID: ")
-    client_secret = input("Enter your client secret: ")
-    prefix_url = input("Enter the prefix for API URLs: ")
-    rules_xml_path = input("Enter the path to the rules XML file: ")
-    choise = input("Do you wanna run CPILint(Run this code in the folder with the iflows(files))? [y/n]: ")
-    api_url_get_packs = prefix_url + "/api/v1/IntegrationPackages"
-    api_url_get_iflow = prefix_url + "/api/v1/IntegrationPackages('{id}')/IntegrationDesigntimeArtifacts"
-    api_url_download = prefix_url + "/api/v1/IntegrationDesigntimeArtifacts(Id='{id}',Version='{version}')/$value"
-    version_value = "active"
-    iflow_names_new = []
+    choose = input("Choose 1 to integration suite / Choose 2 to NEO CPI: ")
+    if choose == "1":
+        auth_api_url = input("Enter the authentication API URL: ")
+        client_id = input("Enter your client ID: ")
+        client_secret = input("Enter your client secret: ")
+        prefix_url = input("Enter the prefix for API URLs: ")
+        rules_xml_path = input("Enter the path to the rules XML file: ")
+        choise = input("Do you wanna run CPILint(Run this code in the folder with the iflows(files))? [y/n]: ")
+        api_url_get_packs = prefix_url + "/api/v1/IntegrationPackages"
+        api_url_get_iflow = prefix_url + "/api/v1/IntegrationPackages('{id}')/IntegrationDesigntimeArtifacts"
+        api_url_download = prefix_url + "/api/v1/IntegrationDesigntimeArtifacts(Id='{id}',Version='{version}')/$value"
+        version_value = "active"
+        iflow_names_new = []
 
-    bearer_token = get_bearer_token(auth_api_url, client_id, client_secret)
+        bearer_token = get_bearer_token(auth_api_url, client_id, client_secret)
 
-    if bearer_token:
-        package_names = get_package_names(api_url_get_packs, bearer_token)
+        if bearer_token:
+            package_names = get_package_names(api_url_get_packs, bearer_token)
+            with ThreadPoolExecutor(max_workers=5) as executor:
+                for package_name in package_names:
+                    iflow_names = get_iflows_names(package_name, api_url_get_iflow, bearer_token)
+                    iflow_names_new.extend(iflow_names)
+                    for iflow_name in iflow_names:
+                        executor.submit(download_zip_file, api_url_download, iflow_name, version_value, bearer_token)
+
+            if choise == "y":
+                run_cpi_lint(iflow_names_new, rules_xml_path)
+    if choose == "2":
+        
+        iflow_names_new = []
+        
+        s_user          = input("Enter your S-User: ")
+        password        = input("Enter your Password: ")
+        tenant          = input("Enter the Tenant URL: ")
+        choise          = input("Do you wanna run CPILint(Run this code in the folder with the iflows(files))? [y/n]: ")
+        rules_xml_path  = input("Enter the path to the rules XML file: ")
+        
+        api_url_get_packs = tenant + "/api/v1/IntegrationPackages"
+        api_url_get_iflow = tenant + "/api/v1/IntegrationPackages('{id}')/IntegrationDesigntimeArtifacts"
+        api_url_download  = tenant + "/api/v1/IntegrationDesigntimeArtifacts(Id='{id}',Version='{version}')/$value"
+        version_value = "active"
+        
+        package_names = get_package_names_neo(api_url_get_packs, s_user, password)
         with ThreadPoolExecutor(max_workers=5) as executor:
             for package_name in package_names:
-                iflow_names = get_iflows_names(package_name, api_url_get_iflow, bearer_token)
+                iflow_names = get_iflows_names_neo(package_name, api_url_get_iflow, s_user, password)
                 iflow_names_new.extend(iflow_names)
                 for iflow_name in iflow_names:
-                    executor.submit(download_zip_file, api_url_download, iflow_name, version_value, bearer_token)
-
+                        executor.submit(download_zip_file_neo, api_url_download, iflow_name, version_value, s_user, password)
         if choise == "y":
-            run_cpi_lint(iflow_names_new, rules_xml_path)
+            run_cpi_lint(iflow_names_new, rules_xml_path)            
+
+        
